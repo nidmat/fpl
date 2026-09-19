85a649950641b224c64d0baee20b4e5853298742
nidmat <40243367+nidmat@users.noreply.github.com>
2026-09-19 00:40:29 -0500

    Delete pages directory

--- pages/1_📊_FPL_Ownership.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from fpl_utils import render_option2_page
-
-st.set_page_config(page_title="FPL Ownership", page_icon="📊", layout="wide")
-render_option2_page("fpl_stats.xlsx", "FPL Ownership")
